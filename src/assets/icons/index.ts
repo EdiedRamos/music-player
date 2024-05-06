488b2cd4b61dd19c818f8c649bbaae74b42d70ba
EdiedRamos <edieisa22@gmail.com>
2024-05-06 03:12:24 -0500

    chore: add control icons

---
+++ src/assets/icons/index.ts
@@ -0,0 +1,4 @@
+export { default as PlayIcon } from "./Play_fill.svg";
+export { default as StopIcon } from "./Stop_fill.svg";
+export { default as StopPlayR } from "./Stop_and_play_fill_reverse.svg";
+export { default as StopPlay } from "./Stop_and_play_fill-normal.svg";
--- src/components/PlayerControl.tsx
+++ src/components/PlayerControl.tsx
@@ -1,7 +1,16 @@
+import { PlayIcon, StopIcon, StopPlay, StopPlayR } from "../assets/icons";
+
 export const PlayerControl = () => {
   return (
     <div>
-      <div></div>
+      <div>
+        <img src={StopPlay} />
+        <div>
+          <img src={PlayIcon} />
+          <img src={StopIcon} />
+        </div>
+        <img src={StopPlayR} />
+      </div>
     </div>
   );
 };
